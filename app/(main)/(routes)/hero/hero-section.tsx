01f43e5fa164aa3a1dc5c2860c99fb9de56958e7
roma-frontend <romangulanyan@gmail.com>
2023-12-03 18:36:06 +0400

    added new directory in gitignore

--- app/(main)/(routes)/hero/hero-section.tsx
+++ app/(main)/(routes)/hero/hero-section.tsx
@@ -38,20 +38,6 @@ const imageVariants = {
     },
   },
 };
-const carVariants = {
-  initial: {
-    x: 500,
-    opacity: 0,
-  },
-  animate: {
-    x: 0,
-    opacity: 1,
-    transition: {
-      duration: 1,
-      staggerChildren: 0.1,
-    },
-  },
-};
 
 const HeroSection = ({ id }: { id: string }) => {
   return (
